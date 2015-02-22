1de118fd9eaef3b662fb5a60445a7df93a8f5092
maaybars@gmail.com <maaybars@gmail.com@6092864b-5ed8-bf46-d640-01988c1c3b4e>
2015-02-22 00:06:12 +0000

    Entrada inicial

---
+++ arbi/IRControl.ino
@@ -0,0 +1,92 @@
+
+void process_IR(void)
+{
+  command = CMD_NONE;
+  decode_results results;
+  if (irrecv.decode(&results)) 
+  {
+    if (results.value == KEY_2 )
+    {
+      command = CMD_FW;
+      iKey = KEY_2;
+    }
+    else if (results.value == KEY_4)
+    {
+      if (left_run == FORWARD || right_run == FORWARD) command = CMD_FW_LEFT;
+      else if (left_run == BACKWARD || right_run == BACKWARD) command = CMD_BW_LEFT;
+      else 
+      {
+        command = CMD_FW_LEFT;
+        b_no_move = true;
+      }
+      iKey = KEY_4;
+    }
+    else if (results.value == KEY_5)
+    {
+      command = CMD_STOP;
+      iKey = KEY_5;
+    }
+    else if (results.value == KEY_6)
+    {
+      if (left_run == FORWARD || right_run == FORWARD) command = CMD_FW_RIGHT;
+      else if (left_run == BACKWARD || right_run == BACKWARD) command = CMD_BW_RIGHT;
+      else 
+      {
+        command = CMD_FW_RIGHT;
+        b_no_move = true;
+      }
+      iKey = KEY_6;
+    } 
+    else if (results.value == KEY_MENOS)
+    {
+      command = CMD_DECREASE;
+      iKey = KEY_MENOS;
+    }  
+    else if (results.value == KEY_MAS)
+    {
+      command = CMD_INCREASE;
+      iKey = KEY_MAS;
+    } 
+    else if (results.value == KEY_8)
+    {
+      command = CMD_BW;
+      iKey = KEY_8;
+    } 
+   else if (results.value == 4294967295)
+    {
+      timer =7000; 
+      if ((iKey != KEY_MENOS) & (iKey != KEY_MAS)) 
+      {
+        b_pulsacion_larga = true;
+        //Serial.println("pulsacion larga\n");
+      }
+    } 
+     else
+    {
+      command = CMD_NONE;
+      //Serial.println(results.value, HEX);
+      //Serial.println(results.value);
+    } 
+    irrecv.resume(); // Receive the next value
+  }
+  else  // no code received
+  {
+    if (b_pulsacion_larga == true)  //Si hay pulsacion larga
+    {
+      if (timer > 0) --timer;      // aun se recibe pulsacion larga
+      else                        // ya no rebimos pulsacion larga
+      {
+        b_pulsacion_larga = false;
+        //Serial.println(left_run,right_run);
+        if (b_no_move == true){b_no_move=false;command = CMD_STOP;}
+        else if (left_run == FORWARD && right_run == FORWARD) command = CMD_STOP;
+        else if (left_run == BACKWARD && right_run == BACKWARD) command = CMD_STOP;
+        else if (left_run == FORWARD || right_run == FORWARD) command = CMD_FW;
+        else if (left_run == BACKWARD || right_run == BACKWARD) command = CMD_BW;
+        else
+          command = CMD_NONE;
+      }
+    }
+  }
+}
+
