69fdd5f4649a57f5b9c86ea6d285d39e2d8564d8
maaybars@gmail.com <maaybars@gmail.com@6092864b-5ed8-bf46-d640-01988c1c3b4e>
2015-03-01 17:17:44 +0000

    Nuevas constantes

--- arbi/RobotConstants.h
+++ arbi/RobotConstants.h
@@ -21,14 +21,47 @@
 #define KEY_8  16730805
 
 #define MID_MOVE_SONAR  1590
-#define RANGE_SONAR  1180
-#define MAX_MOVE_SONAR  MID_MOVE_SONAR +  RANGE_SONAR/2  //2250
-#define MIN_MOVE_SONAR  MID_MOVE_SONAR -  RANGE_SONAR/2    //750
-#define INC_MOVE_SONAR  5
+#define MAX_RANGE_SONAR  1180
+#define CHECK_RANGE_SONAR  450
+#define MAX_MOVE_SONAR  MID_MOVE_SONAR +  MAX_RANGE_SONAR/2  //2250
+#define MIN_MOVE_SONAR  MID_MOVE_SONAR -  MAX_RANGE_SONAR/2    //750
 
-//const int MIN_SPEED = 60; // first table entry is 60% speed
-//const int SPEED_TABLE_INTERVAL = 10; // each table entry is 10% faster speed
-//const int NBR_SPEEDS =  1 + (100 - MIN_SPEED)/ SPEED_TABLE_INTERVAL;
- 
-//int speedTable[NBR_SPEEDS]  =  {60,   70,   80,   90,  100}; // speeds  
-//int rotationTime[NBR_SPEEDS] = {5500, 3300, 2400, 2000, 1750}; // time
+#define LEFT_MOVE_SONAR  MID_MOVE_SONAR +  CHECK_RANGE_SONAR/2  //2250
+#define RIGHT_MOVE_SONAR  MID_MOVE_SONAR -  CHECK_RANGE_SONAR/2    //750
+#define INC_MOVE_SONAR  10
+
+// defines for directions
+const int DIR_LEFT   = 0;
+const int DIR_RIGHT  = 1;
+const int DIR_CENTER = 2; 
+
+const int LED_PIN = 13;
+const int RECV_PIN = 8;
+const int SERVO_PIN = 9;  //servo de movimiento de SONAR1
+
+#define SONAR1  0  // sensor ultrasonico delantero
+#define SONAR2  1 // sensor ultrasonico trasero
+
+const int SONAR1_TRIG_PIN = 5;  //Trigger pin of HC-SR04
+const int SONAR1_ECHO_PIN = 4;  //Echo pin of HC-SR04
+const int SONAR2_TRIG_PIN = 3;  //Trigger pin of HC-SR04
+const int SONAR2_ECHO_PIN = 2;  //Echo pin of HC-SR04
+
+const float Drag = 2.0; // coeficieente de rozamiento del suelo
+
+enum {MOV_LEFT, MOV_RIGHT, MOV_FORWARD,
+      MOV_BACK, MOV_ROTATE, MOV_STOP};
+      
+const char* states[] = {"Left", "Right", "Forward", 
+                        "Back", "Rotate", "Stop"};
+                        
+const int  MIN_DISTANCE = 20;     // robot stops when object is nearer (in cm)
+const int  CLEAR_DISTANCE = 60;  // distance in cm considered attractive to move
+
+enum {DATA_start, DATA_LEFT, DATA_CENTER, DATA_RIGHT,DATA_DRIFT,DATA_DISTANCE,
+     DATA_nbrItems};
+
+char* labels[]= {"","Left Line","Center Line","Right Line","Drift","Distance"};
+int minRange[]= { 0,          0,            0,           0,  -1023,        0 };  
+int maxRange[]= { 0,       1023,         1023,        1023,   1023,       144};  
+
